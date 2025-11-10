f7623e18dd468e1782917b724c24dc017583401b
simonlam <simon@fortuna-online.org>
2025-11-10 12:03:38 +0800

    Updated ProjectBoard.tsx

--- src/components/ProjectBoard.tsx
+++ src/components/ProjectBoard.tsx
@@ -1056,7 +1056,7 @@ export function ProjectBoard() {
                   </>
                 )}
               </div>
-              {!isClientSection && !isAdminSection && isFundingProjectType && (
+              {!isClientSection && !isAdminSection && !isComSecSection && isFundingProjectType && (
                 <div className="flex items-center gap-3">
                   <div className="relative">
                     <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-slate-400" />
