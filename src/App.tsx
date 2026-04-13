31a563c9b185ced0fe7828c10912aefc4f060df7
simonlam <simon@fortuna-online.org>
2026-04-13 10:13:33 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function AppContent() {
   }, [user]);
 
   useEffect(() => {
-    if (user && !loading && window.location.pathname !== '/onboarding' && window.location.pathname !== '/phone-scan' && user.id !== '7e8c51af-9590-4a7f-8e6f-545d6a7c8412') {
+    if (user && !loading && window.location.pathname !== '/onboarding' && window.location.pathname !== '/phone-scan') {
       const shouldShowTaskSummary = () => {
         const now = new Date();
         const sessionKey = `task_summary_session_${new Date().toDateString()}`;
@@ -282,7 +282,7 @@ function AppContent() {
   return (
     <>
       {user ? <ProjectBoard /> : <LoginPage />}
-      {showTaskSummary && user && user.id !== '7e8c51af-9590-4a7f-8e6f-545d6a7c8412' && (
+      {showTaskSummary && user && (
         <TaskNotificationModal
           onClose={() => setShowTaskSummary(false)}
         />
